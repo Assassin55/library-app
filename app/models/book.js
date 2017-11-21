f1dd961b74a885c3b5571d730fecd419686ba4e7
Assassin55 <borna.kc@gmail.com>
2017-11-21 22:02:11 +0100

    Gotovooo!

--- app/models/book.js
+++ app/models/book.js
@@ -2,12 +2,18 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 import Faker from 'faker';
+import Ember from 'ember';
+
+const { computed } = Ember;
 
 export default Model.extend({
   title: attr('string'),
   releaseYear: attr('date'),
   library: belongsTo('library', { inverse: 'books', async: true }),
   author: belongsTo('author', { inverse: 'books', async: true }),
+
+  isEditing: false,
+  isNotValid: computed.empty('title'),
 
   randomize(author, library) {
     this.set('title', this._bookTitle());
--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,35 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  bookInEdit: '',
+
+  model() {
+    return this.store.findAll('book');
+  },
+
+  actions: {
+    editBook(book) {
+      book.set('isEditing', true);
+      this.set('bookInEdit', book);
+    },
+
+    saveBook(book) {
+      book.save();
+      book.set('isEditing', false);
+      this.set('bookInEdit', '');
+    },
+
+    cancelBookEdit(book) {
+      if (this.get('bookInEdit') != '') {
+        book.rollbackAttributes();
+        book.set('isEditing', false);
+        this.set('bookInEdit', '');
+      }
+    },
+
+    willTransition() {
+      this.send('cancelBookEdit', this.get('bookInEdit'));
+    }
+  }
 });
